44e6a6920244eb52760ba9b74d4d1acc5ba9ad92
Taimuraz <parabotic@gmail.com>
2017-12-09 17:10:17 +0300

    Java Loops 1 & 2!

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -17,4 +17,38 @@ public class HackerRank {
         }
         System.out.println("================================");
     }
+
+    public static void javaLoops_1(){
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for( int i = 0; i < 10; i ++){
+            System.out.printf("%d x %d = %d%n", N, i+1, N * (i+1));
+        }
+
+    }
+
+    public static void javaLoops_2(){
+        Scanner in = new Scanner(System.in);
+        int sum = 0;
+        int pow = 1;
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            sum += a + b;
+            System.out.printf("%d ",sum);
+            for(int j=1;j<n;j++){
+                for(int k=0;k<j;k++)
+                    pow *= 2;
+                sum += pow*b;
+                System.out.printf("%d ",sum);
+                pow = 1;
+            }
+            sum = 0;
+            System.out.printf("%n");
+        }
+        in.close();
+
+    }
 }
